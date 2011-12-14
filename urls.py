ccde5662e5bcef5f257efe0838fb452daa867f17
Krzysztof Olszewski <k.olszewski@o2.pl>
2011-12-14 08:37:55 +0100

    ok14

--- urls.py
+++ urls.py
@@ -1,11 +1,2 @@
 from django.conf.urls.defaults import *
-from satchmo_utils.urlhelper import replace_urlpattern
-from product.urls.category import urlpatterns
-
-replacement=patterns('product.views',
-    (r'^(?P<parent_slugs>([-\w]+/)*)?(?P<slug>[-\w]+)/$',
-        'category_view_kamel', {}, 'satchmo_category'),
-    (r'^$', 'category_index', {}, 'satchmo_category_index'),
-)
-replace_urlpattern(urlpatterns,replacement)
-
+from satchmo_store.urls import urlpatterns
